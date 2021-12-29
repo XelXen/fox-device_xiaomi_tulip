f706811980f6911ac3ea05d8ce4996824d9a12ae
Sushrut1101 <guptasushrut@gmail.com>
2021-12-29 18:07:13 +0530

    tulip: Ensure that the Build Vars Get Exported
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2018-2020 The OrangeFox Recovery Project
+# 	Copyright (C) 2028-2021 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -18,13 +18,29 @@
 # 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="tulip"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-        export PLATFORM_VERSION="16.1.0"
+    export PLATFORM_VERSION="16.1.0"
    	export TW_DEFAULT_LANGUAGE="en"
-        export OF_SCREEN_H="2280";
-        export OF_STATUS_H="80"
-        export OF_STATUS_INDENT_LEFT="48"
-        export OF_STATUS_INDENT_RIGHT="48"
+    export OF_SCREEN_H="2280";
+    export OF_STATUS_H="80"
+    export OF_STATUS_INDENT_LEFT="48"
+    export OF_STATUS_INDENT_RIGHT="48"
 	export OF_USE_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 	export OF_REDUCE_DECRYPTION_TIMEOUT=1
